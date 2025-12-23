137339fad78aa8baeeb48439d44450206832d1cc
Namito Yokota <namitoyokota@icloud.com>
2025-12-22 18:29:11 -0600

    Remove local storage.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(window).on('load', function () {
     var $input = $('#m3u8-placeholder');
     var params = new URLSearchParams(window.location.search);
-    var initial = params.get('url') || localStorage.getItem('m3u8-link') || '';
+    var initial = params.get('url') || '';
     $input[0].value = initial;
 
     $input.on('input', function () {
@@ -16,7 +16,6 @@ $(window).on('load', function () {
     $('#form').on('submit', function (e) {
         e.preventDefault();
         var val = $input[0].value;
-        localStorage.setItem('m3u8-link', val);
         var p = new URLSearchParams(window.location.search);
         if (val) p.set('url', val);
         else p.delete('url');
